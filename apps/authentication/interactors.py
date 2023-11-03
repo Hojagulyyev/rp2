53e1d634ac28137d26ce2dbdc1be6446a360d632
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-03 21:03:07 +0500

    feat(auth): set interactors to forms actions

--- apps/authentication/interactors.py
+++ apps/authentication/interactors.py
@@ -16,16 +16,16 @@ def signup(request):
 
     # ===== DTO
 
-    username = request.POST.get('username', '').strip()
-    password = request.POST.get('password', '').strip()
-    repeat_password = request.POST.get('repeat_password', '').strip()
-    first_name = request.POST.get('first_name', '').strip()
-    last_name = request.POST.get('last_name', '').strip()
+    username = request.POST.get('signup_username', '').strip()
+    password = request.POST.get('signup_password', '').strip()
+    repeat_password = request.POST.get('signup_repeat_password', '').strip()
+    first_name = request.POST.get('signup_first_name', '').strip()
+    last_name = request.POST.get('signup_last_name', '').strip()
 
     # ===== VALIDATION
 
     if password != repeat_password:
-        messages.error(request, "These credentials are incorrect")
+        messages.error(request, "Passwords are mismatching")
         return redirect(
             f"{reverse('authentication:authentication_view')}"
             f"?username={username}"
@@ -33,20 +33,27 @@ def signup(request):
 
     # ===== PROCESS
 
-    user = User.objects.create_user(
-        username=username,
-        password=password,
-        first_name=first_name,
-        last_name=last_name,
-    )
+    try:
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+    except Exception as e:
+        messages.error(request, str(e))
+        return redirect(
+            f"{reverse('authentication:authentication_view')}"
+        )
+
     Account.objects.create(user=user)
 
     return redirect('authentication:authentication_view')
 
 
 def signin(request):
-    username = request.POST.get('username', '').strip()
-    password = request.POST.get('password', '').strip()
+    username = request.POST.get('signin_username', '').strip()
+    password = request.POST.get('signin_password', '').strip()
 
     user = authenticate(username=username, password=password)
 
