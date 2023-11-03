b8455ba6e68d6d7f196e2708c6680618bb9728a8
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-03 20:10:13 +0500

    feat(issue): make model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -11,4 +11,3 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user
-    
--- apps/issues/models.py
+++ apps/issues/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import Account
+from apps.clans.models import Clan
+
+
+class Issue(models.Model):
+
+    author = models.ForeignKey(Account, on_delete=models.SET_NULL, blank=True, null=True)
+    assignees = models.ManyToManyField(Account)
+    reviewers = models.ManyToManyField(Account)
+    clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=64, blank=True, null=True)
+    priority = models.CharField(max_length=64, blank=True, null=True)
+    status = models.CharField(max_length=64, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
