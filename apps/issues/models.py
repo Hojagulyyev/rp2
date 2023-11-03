c3e98217a4e9b2051c520c0f2abb6c48b71df41b
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-03 22:30:40 +0500

    chore(issue): add is_closed field

--- apps/issues/models.py
+++ apps/issues/models.py
@@ -17,6 +17,7 @@ class Issue(models.Model):
     status = models.CharField(max_length=64, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_date = models.DateField(default=timezone.now)
+    is_closed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
