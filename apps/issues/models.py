931a8d307615221e093d1684c016dce8070f996d
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-03 20:33:59 +0500

    fix(issue): eliminate duplicate names associated with an account

--- apps/issues/models.py
+++ apps/issues/models.py
@@ -7,8 +7,8 @@ from apps.clans.models import Clan
 class Issue(models.Model):
 
     author = models.ForeignKey(Account, on_delete=models.SET_NULL, blank=True, null=True)
-    assignees = models.ManyToManyField(Account)
-    reviewers = models.ManyToManyField(Account)
+    assignees = models.ManyToManyField(Account, related_name="issues_from_assignee")
+    reviewers = models.ManyToManyField(Account, related_name="issues_from_reviewer")
     clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=64, blank=True, null=True)
