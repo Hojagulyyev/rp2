b320c52f518e36ddde5299606bdb604131977820
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-03 22:06:21 +0500

    fix: add SITE_ID constant

--- rp2/settings.py
+++ rp2/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SITE_ID = 1
