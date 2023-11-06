8588489889b4bec60ec7c70e229d24dbe66646e2
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-06 13:22:44 +0500

    feat(issue): make pagination

--- apps/issues/views.py
+++ apps/issues/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from rp2.pagination import paginate
+
 from ..issues.models import Issue
 
 
@@ -7,14 +9,19 @@ def issues(request):
 
     account = request.user.account
 
+    # ===== Input DTO
+
+    page = request.GET.get("page", 1)
+
     # ===== Output DTO
 
     issue_queryset = Issue.objects.filter(
         clan=account.clan,
     )
+    paginated_issue_queryset = paginate(issue_queryset, page, 2)
 
     context = {
-        "issues": issue_queryset,
+        "issues": paginated_issue_queryset,
     }
 
     return render(request, "issues/issues.html", context)
