0813039ce44dfd78b853b4e1358c2446caa5dc9f
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-09 17:50:33 +0500

    chore(account): add level, xp fields into model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,5 +9,9 @@ class Account(models.Model):
     user: User = models.OneToOneField(User, on_delete=models.CASCADE)
     clan = models.ForeignKey(Clan, on_delete=models.CASCADE, blank=True, null=True)
 
+    level = models.IntegerField(default=1)
+    xp = models.IntegerField(default=0)
+    required_xp_for_next_level = models.IntegerField(default=100)
+
     def __str__(self):
         return self.user.username
