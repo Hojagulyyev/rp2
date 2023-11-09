b5b5212a36f2974c02f6edccd51018847538f285
Hojagulyyev <ovez.hojagulyyev@gmail.com>
2023-11-09 19:59:04 +0500

    fix(diary): add diary field into comment model

--- apps/diaries/models.py
+++ apps/diaries/models.py
@@ -9,6 +9,9 @@ class Diary(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     created_date = models.DateField(default=timezone.now)
 
+    class Meta:
+        verbose_name_plural = "Diaries"
+
     def __str__(self):
         return f"{self.account} - {self.created_date}"
 
@@ -18,14 +21,21 @@ class DiaryCommit(models.Model):
     diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
     message = models.TextField()
 
+    class Meta:
+        verbose_name_plural = "Diary Commits"
+
     def __str__(self):
         return self.message
 
 
 class DiaryComment(models.Model):
 
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
     author = models.ForeignKey(Account, on_delete=models.CASCADE)
     body = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Diary Comments"
 
     def __str__(self):
         return self.body
